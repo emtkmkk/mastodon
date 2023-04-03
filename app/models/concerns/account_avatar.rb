512818f8047878250038f3842c005635d703cedd
emtkmkk <65014664+emtkmkk@users.noreply.github.com>
2023-04-03 20:45:05 +0900

    Update account_avatar.rb

--- app/models/concerns/account_avatar.rb
+++ app/models/concerns/account_avatar.rb
@@ -9,7 +9,7 @@ module AccountAvatar
   class_methods do
     def avatar_styles(file)
       styles = { original: { geometry: '400x400#', file_geometry_parser: FastGeometryParser } }
-      styles[:static] = { geometry: '400x400#', format: 'png', convert_options: '-coalesce', file_geometry_parser: FastGeometryParser } if file.content_type == 'image/gif'
+      styles[:static] = { geometry: '400x400#', format: 'png', convert_options: '-coalesce', file_geometry_parser: FastGeometryParser } if file.content_type == 'image/gif' || file.content_type == 'image/webp'
       styles
     end
 
@@ -18,7 +18,11 @@ module AccountAvatar
 
   included do
     # Avatar upload
-    has_attached_file :avatar, styles: ->(f) { avatar_styles(f) }, convert_options: { all: '+profile "!icc,*" +set modify-date +set create-date' }, processors: [:lazy_thumbnail]
+    if file.content_type == 'image/webp'
+      has_attached_file :avatar, styles: ->(f) { avatar_styles(f) }, convert_options: { all: '+profile "!icc,*" +set modify-date +set create-date' }, processors: [:webp_transcoder]
+    else
+      has_attached_file :avatar, styles: ->(f) { avatar_styles(f) }, convert_options: { all: '+profile "!icc,*" +set modify-date +set create-date' }, processors: [:lazy_thumbnail]
+    end
     validates_attachment_content_type :avatar, content_type: IMAGE_MIME_TYPES
     validates_attachment_size :avatar, less_than: LIMIT
     remotable_attachment :avatar, LIMIT, suppress_errors: false
@@ -29,6 +33,6 @@ module AccountAvatar
   end
 
   def avatar_static_url
-    avatar_content_type == 'image/gif' ? avatar.url(:static) : avatar_original_url
+    avatar_content_type == 'image/gif' || file.content_type == 'image/webp'  ? avatar.url(:static) : avatar_original_url
   end
 end
