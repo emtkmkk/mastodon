099d3444c6c8ed819388cb68f6bd7802780b2676
emtkmkk <65014664+emtkmkk@users.noreply.github.com>
2023-04-04 09:07:29 +0900

    Update account_avatar.rb

--- app/models/concerns/account_avatar.rb
+++ app/models/concerns/account_avatar.rb
@@ -9,6 +9,7 @@ module AccountAvatar
   class_methods do
     def avatar_styles(file)
       styles = { original: { geometry: '400x400#', file_geometry_parser: FastGeometryParser } }
+      styles = { original: { geometry: '400x400#', format: 'gif', file_geometry_parser: FastGeometryParser } } if file.content_type == 'image/webp'
       styles[:static] = { geometry: '400x400#', format: 'png', convert_options: '-coalesce', file_geometry_parser: FastGeometryParser } if file.content_type == 'image/gif' || file.content_type == 'image/webp'
       styles
     end
